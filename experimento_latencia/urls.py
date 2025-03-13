18aea73ada1d9c5e1a3eaaa707f64623d112486f
DilanJJJ <dj.escobar@uniandes.edu.co>
2025-03-13 16:02:38 -0500

    healthCheck

--- experimento_latencia/urls.py
+++ experimento_latencia/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('health-check/', views.healthCheck),
     path('api/ingesta/', include('ingesta_datos.urls')),
     path('api/procesamiento/', include('procesamiento.urls')),
 ]
